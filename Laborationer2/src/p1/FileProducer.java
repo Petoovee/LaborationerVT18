fad40542c9f7f9825313093b3616c881e6a0d793
Petoovee <Petomatick@gmail.com>
2018-04-28 09:07:36 +0200

    Init copy

---
+++ Laborationer2/src/p1/FileProducer.java
@@ -0,0 +1,29 @@
+package p1;
+
+import javax.swing.Icon;
+
+public class FileProducer implements IconProducer {
+	
+	public FileProducer(String filename) {
+	}
+	
+	public int delay() {
+		return 0;
+	}
+
+	@Override
+	public int times() {
+		return 0;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public Icon nextIcon() {
+		return null;
+	}
+
+}
---
+++ Laborationer2/src/p1/IconProducerManager.java
@@ -0,0 +1,15 @@
+package p1;
+
+public class IconProducerManager
+{
+	private Buffer<IconProducer> buffer;
+	public IconProducerManager(Buffer<IconProducer> bufer)
+	{
+		this.buffer = buffer;
+	}
+	
+	public void addIconProducer(IconProducer ic)
+	{
+		buffer.put(ic);
+	}
+}
